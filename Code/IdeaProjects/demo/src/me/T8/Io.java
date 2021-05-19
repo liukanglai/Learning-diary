1a395525f75ef7f302bfc93cffce779212ead136
2821756507@qq.mail <2821756507@qq.com>
2021-05-19 09:16:33 +0800

    java garbage

--- Code/IdeaProjects/demo/src/me/T8/Io.java
+++ Code/IdeaProjects/demo/src/me/T8/Io.java
@@ -1,8 +1,21 @@
 package me.T8;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author liukanglai
  * @date 5/15/21 - 11:25 AM
  */
 public class Io {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("/home/liukanglai/1.txt");
+        int read = fr.read();
+        while(read != -1) {
+            System.out.println((char) read);
+            read = fr.read();
+        }
+        fr.close();
+    }
 }
